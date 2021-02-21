b3ebf072a919af3b8b826e8d93c41295ba6686a8
Bartosz Trusinski <bartosztrusinski@gmail.com>
2021-02-21 21:44:38 +0100

    create campground new and create

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
     res.render('home');
 })
@@ -29,10 +31,20 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', {campgrounds: allCampgrounds});
 })
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+
 app.get('/campgrounds/:id', async (req, res) => {
     const {id} = req.params;
     const foundCampground = await Campground.findById(id);
     res.render('campgrounds/show', {campground: foundCampground});
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const newCampground = new Campground(req.body.campground);
+    await newCampground.save();
+    res.redirect(`/campgrounds/${newCampground._id}`);
 })
 
 app.listen(3000, () => {
