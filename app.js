339dd1a1faf8ea474df34c130ebe75694a17acc6
Bartosz Trusinski <bartosztrusinski@gmail.com>
2021-02-22 15:32:13 +0100

    add ejs engine and create layouts

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const engine = require('ejs-mate');
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -18,11 +19,13 @@ db.once('open', () => {
 
 const app = express();
 
+app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({extended: true}));
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
+
 
 app.get('/', (req, res) => {
     res.render('home');
