0f2a87d89a31e122e2cf54fa84b57a950756b7d8
Bartosz Trusinski <bartosztrusinski@gmail.com>
2021-02-22 16:03:51 +0100

    update campground model

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,17 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            price,
+            description: 'Lee parley pillage ye furl bilged on her anchor aft clap of thunder lookout provost. Ballast hardtack trysail gun pirate bounty fire ship hornswaggle Davy Jones\' Locker jolly boat. Reef sails maroon lookout case shot rope\'s end Sail ho interloper lee port hail-shot.\n' +
+                '\n' +
+                'Holystone pressgang cutlass barque haul wind deadlights line Pirate Round fire in the hole walk the plank. Pillage scurvy parley to go on account Sea Legs aye sloop Pirate Round draft mutiny. Galleon square-rigged boatswain reef six pounders hulk hearties lookout schooner transom.\n' +
+                '\n' +
+                'Log prow nipperkin spanker Yellow Jack walk the plank list interloper cutlass hempen halter. Davy Jones\' Locker main sheet sloop smartly skysail six pounders snow jack hempen halter loaded to the gunwalls. Mizzenmast hulk quarter chase guns trysail avast long clothes squiffy grog dance the hempen jig.'
         });
         await camp.save();
     }
