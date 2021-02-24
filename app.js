fcf728f00c8eb88ac4fbd76d3cf2682056b7af81
Bartosz Trusinski <bartosztrusinski@gmail.com>
2021-02-24 20:15:19 +0100

    delete camp along with reviews middleware

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
 
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const {id} = req.params;
-    const foundCampground = await Campground.findById(id);
+    const foundCampground = await Campground.findById(id).populate('reviews');
     res.render('campgrounds/show', {campground: foundCampground});
 }))
 
@@ -103,6 +103,13 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res)
     await review.save();
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync( async (req, res) => {
+    const {id, reviewId} = req.params;
+    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
 }))
 
 app.all('*', (req, res, next) => {
--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
 const campgroundSchema = new Schema({
@@ -14,6 +15,16 @@ const campgroundSchema = new Schema({
         }
     ]
 });
+
+campgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
 const Campground = mongoose.model('Campground', campgroundSchema);
 
