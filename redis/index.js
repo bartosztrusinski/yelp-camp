46ed2293f5194980668dc07d4b019d68b06e871a
Bartosz Trusiński <bartosztrusinski@gmail.com>
2023-03-14 16:58:42 +0100

    use env var for port & refactor

--- redis/index.js
+++ redis/index.js
@@ -1,11 +1,11 @@
-const session = require('express-session')
-    , redis = require('redis');
-
-module.exports.redisClient = redis.createClient({
-    host: process.env.REDIS_HOST,
-    port: 10268,
-    password: process.env.REDIS_PASSWORD
+const session = require('express-session');
+const redis = require('redis');
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
 });
+const RedisStore = require('express-brute-redis');
+const RedisSessionStore = require('connect-redis')(session);
 
-module.exports.RedisStore = require('express-brute-redis');
-module.exports.RedisSessionStore = require('connect-redis')(session);
+module.exports = { redisClient, RedisStore, RedisSessionStore };
